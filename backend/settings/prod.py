d915d7cf5758679a882dddcfcf2fbe662983891d
Kopano Kaas Nielsen <knielsen@gov.bw>
2025-10-02 19:50:59 +0200

    Align production staticfiles configuration

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -2,6 +2,8 @@
 
 from __future__ import annotations
 
+from django.conf import global_settings
+
 from .base import *  # noqa: F401,F403
 from .base import (
     BASE_DIR,
@@ -18,5 +20,11 @@ SECRET_KEY = get_secret_key(DEBUG)
 ALLOWED_HOSTS = build_allowed_hosts(["localhost", "127.0.0.1", "0.0.0.0"])
 CSRF_TRUSTED_ORIGINS = build_csrf_trusted_origins(ALLOWED_HOSTS)
 
-STATICFILES_DIRS = []
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+STORAGES = {
+    **getattr(global_settings, "STORAGES", {}),
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
