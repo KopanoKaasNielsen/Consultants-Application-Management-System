a0cef073e02dbf93e6ff598ab4dfa97054d90530
Kopano Kaas Nielsen <knielsen@gov.bw>
2025-10-07 11:54:28 +0200

    Add board dashboard view for submitted consultant applications

--- apps/users/context_processors.py
+++ apps/users/context_processors.py
@@ -1,6 +1,6 @@
 """Custom context processors for the users app."""
 
-from apps.users.views import _user_is_admin
+from apps.users.views import _user_is_admin, _user_is_board_or_staff
 
 
 def user_is_admin(request):
@@ -9,4 +9,12 @@ def user_is_admin(request):
         "user_is_admin": _user_is_admin(request.user)
         if request.user.is_authenticated
         else False,
+    }
+
+
+def user_is_board_or_staff(request):
+    """Expose whether the user can access the board dashboard."""
+
+    return {
+        "user_is_board_or_staff": _user_is_board_or_staff(request.user)
     }
